f004bdf94858338f0b8d699014e3985c4678f87a
Lauren Baggett <81193849+laurenbaggett@users.noreply.github.com>
2024-05-29 18:03:38 -0700

    change save name
    small fix to optimize for practice data

--- DOA_small_aperture/Analyze_detections.m
+++ DOA_small_aperture/Analyze_detections.m
@@ -6,4 +6,4 @@ load('F:\Tracking\Erics_detector\SOCAL_E_63\detections\SOCAL_E_63_detections_tra
 DET{1} = det1;
 DET{2} = det2;
  
-% save('F:\SOCAL_E_63_detections_track389_180611_104730.mat', 'DET')
+% save('F:\SOCAL_E_63_detections_track389_180611_104730_cleaned_brushDOA.mat', 'DET')
